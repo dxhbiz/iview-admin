6479256c81f2a373df231e09d2fb46ed22b3160a
2013-20161210PW\Administrator <281419087@qq.com>
2017-07-25 17:19:58 +0800

    调整eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,11 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'spaced-comment': 0,
+    'space-before-function-paren': 0,
+    'semi': 0,
+    'no-multiple-empty-lines': 0,
+    'object-property-newline': 0
   }
 }
